a37cc4e946efb33eeec2450d13879827ffea364c
gulinoztas <51210188+gulinoztas@users.noreply.github.com>
2019-06-06 01:09:30 +0300

    Add files via upload

---
+++ myRandomVal.py
@@ -0,0 +1,22 @@
+import determine_intervals
+from random import random
+def myRandomVal(CurrentVar):
+    rndCategoryLow = float(determine_intervals.VarInterval[CurrentVar,0])
+    rndCategoryUp = float(determine_intervals.VarInterval[CurrentVar,1])
+    k = int(4*random()+1)
+    IntegerVars = False
+
+    if k ==1:
+        myRandomVal = ((rndCategoryUp-rndCategoryLow)/4)*random() + rndCategoryLow
+    elif k ==2:
+        myRandomVal = ((rndCategoryUp - rndCategoryLow) / 4) * random() + (rndCategoryUp - rndCategoryLow)/4 + rndCategoryLow
+    elif k ==3:
+        myRandomVal = ((rndCategoryUp - rndCategoryLow) / 4) * random() + 2*(rndCategoryUp - rndCategoryLow)/4 + rndCategoryLow
+    else:
+        myRandomVal = ((rndCategoryUp - rndCategoryLow) / 4) * random() + 3*(rndCategoryUp - rndCategoryLow)/4 + rndCategoryLow
+    if IntegerVars==False:
+        myRandomVal = round(myRandomVal,0)    #virgülden sonrasını almıyor.
+    return myRandomVal
+#print(myRandomVal)
+#print(rndCategoryLow)
+#print(rndCategoryUp)
