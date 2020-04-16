4016ded42cf51d8af94e1186765846c43df3d2cf
gulinoztas <51210188+gulinoztas@users.noreply.github.com>
2020-04-16 15:15:11 +0300

    Delete strToformula.py

--- strToformula.py
+++
@@ -1,5 +0,0 @@
-import determine_intervals
-def strToformula(st,strVars,myCurrentSolution):
-    for j in range(0,determine_intervals.num_var):
-        st = st.replace(strVars[j], 0.000000000000001+ myCurrentSolution[j])
-    return st
