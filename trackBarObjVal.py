c682fdaf433b4a0510777ad230bed19ea5aa53aa
gulinoztas <51210188+gulinoztas@users.noreply.github.com>
2020-04-16 15:15:20 +0300

    Delete trackBarObjVal.py

--- trackBarObjVal.py
+++
@@ -1,8 +0,0 @@
-
-TrackBarObjVal = 2478127
-TrackBarDeviation = 234234
-valTrackBarObjVal = TrackBarObjVal * 0.10
-TrackBarDeviation = TrackBarDeviation if TrackBarDeviation + TrackBarObjVal <= 10 else 10 - TrackBarObjVal
-valTrackBarDeviation = TrackBarDeviation*0.10
-TrackBarSatisfaction = 10-(TrackBarObjVal-TrackBarDeviation)
-valTrackBarSatisfaction = TrackBarSatisfaction * 0.10
